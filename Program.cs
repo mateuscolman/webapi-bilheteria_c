48ac33b125925f0c5bcc75df091bd1ce5af20d8a
mateus <mateuscolman@gmail.com>
2022-09-13 22:52:06 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using webapi_bilheteria_c.Properties;
 
